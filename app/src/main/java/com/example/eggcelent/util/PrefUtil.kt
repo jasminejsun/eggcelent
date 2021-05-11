3ff14495bc31ededa2d0e0d00ca9f7d6f97f90ab
jasminejsun <71673279+jasminejsun@users.noreply.github.com>
2021-05-11 03:09:06 -0400

    Added functionality to timer

---
+++ app/src/main/java/com/example/eggcelent/util/PrefUtil.kt
@@ -0,0 +1,56 @@
+package com.example.eggcelent.util
+
+import android.content.Context
+import android.preference.PreferenceManager
+import com.example.eggcelent.MainActivity
+
+class PrefUtil {
+    companion object {
+
+        fun getTimerLength(context: Context):Int {
+            //placeholder
+            return 5
+        }
+
+        private const val PREVIOUS_TIMER_LENGTH_SECONDS_ID = "com.example.eggcelent.previous_timer_length"
+
+        fun getPreviousTimerLengthSeconds(context: Context): Long{
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            return preferences.getLong(PREVIOUS_TIMER_LENGTH_SECONDS_ID, 0)
+        }
+
+        fun setPreviousTimerLengthSeconds(seconds: Long, context: Context){
+            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            editor.putLong(PREVIOUS_TIMER_LENGTH_SECONDS_ID, seconds)
+            editor.apply()
+        }
+
+        private const val TIMER_STATE_ID = "com.example.eggcelent.timer_state"
+
+        fun getTimerState(context: Context): MainActivity.TimerState{
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            val ordinal = preferences.getInt(TIMER_STATE_ID, 0)
+            return MainActivity.TimerState.values()[ordinal]
+        }
+
+        fun setTimerState(state: MainActivity.TimerState, context: Context){
+            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            val ordinal = state.ordinal
+            editor.putInt(TIMER_STATE_ID, ordinal)
+            editor.apply()
+        }
+
+        private const val SECONDS_REMAINING_ID = "com.examples.eggcelent.seconds_remaining"
+
+        fun getSecondsRemaining(context: Context): Long{
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            return preferences.getLong(SECONDS_REMAINING_ID, 0)
+        }
+
+        fun setSecondsRemaining(seconds: Long, context: Context){
+            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            editor.putLong(SECONDS_REMAINING_ID, seconds)
+            editor.apply()
+        }
+    }
+}
